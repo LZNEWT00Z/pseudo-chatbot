5d36cbe0e64a74f81483692e6344aba02baa75dd
Anton <antony.kucherov@yandex.ru>
2019-03-04 01:12:43 +0300

    Added a random phrase generator.

--- bot.py
+++ bot.py
@@ -2,3 +2,6 @@ if __name__ == "__main__":
     print("Hello, let's chat!")
 print("I\'m a bot, by the way!")
 print("Also, I\'m very happy!")
+from random import randint
+print("Here are some random messages:")
+[print("One of my lucky numbers is: " + str(randint(1,10))) for i in range(5)]
